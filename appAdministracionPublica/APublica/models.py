33f62e118c94c11edc6a4ba1a1425c8ae22bdf6f
dezkareid <elmaildeldezkareid@gmail.com>
2013-03-18 04:07:01 -0600

    Base de datos

--- appAdministracionPublica/APublica/models.py
+++ appAdministracionPublica/APublica/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+	nombre = models.CharField (blank=True,max_length=200)
+
+	def __unicode__(self):
+		return self.nombre
+
+class Libro(models.Model):
+	nombre = models.CharField (blank=True,max_length=200)
+	creado = models.DateTimeField (auto_now=True)
+	disponible = models.BooleanField (default=True)
+	autor = models.ForeignKey(Autor)
+
+	def __unicode__(self):
+		return self.nombre
