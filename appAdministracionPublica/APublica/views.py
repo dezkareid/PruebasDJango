d5d20635052979ead3719437048e59957f464a2a
dezkareid <elmaildeldezkareid@gmail.com>
2013-03-18 03:20:08 -0600

    Template

--- appAdministracionPublica/APublica/views.py
+++ appAdministracionPublica/APublica/views.py
@@ -1,5 +1,23 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+from datetime import datetime
+from django.shortcuts import render_to_response
 
 def home(request):
 	return HttpResponse("Texto")
+
+def post (request,id):
+	return HttpResponse("Este es el post %i" % int(id))
+
+def hora_actual(request):
+	ahora = datetime.now()
+	return render_to_response('hora.html',{'hora':ahora,'list': range(4)})
+
+def hora_actual2(request):
+	ahora = datetime.now()
+	t = get_template('hora.html')
+	c = Context ({'hora': ahora})
+	html = t.render(c)
+	return HttpResponse(html)
--- appAdministracionPublica/appAdministracionPublica/urls.py
+++ appAdministracionPublica/appAdministracionPublica/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
      url(r'^home/', 'APublica.views.home', name='home'),
+     url(r'^hora_actual/', 'APublica.views.hora_actual', name='hora_actual'),
     # url(r'^appAdministracionPublica/', include('appAdministracionPublica.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
